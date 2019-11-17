b81dd0357a24c09a951540e1174c3ab54a4836a9
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 22:50:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,26 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
+array << 4
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
