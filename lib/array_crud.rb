32963958dc2dee80374c5f8c3c034a0884c42980
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 23:11:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-number = array[index_number]
-number.pop 
+array.delete[index_number]
+array.
 end
